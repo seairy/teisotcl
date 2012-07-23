c251a52b37b186616cc6694f33d7a6eb4f8d2e23
seairy <seairy@gmail.com>
2012-07-23 13:38:36 +0800

    fix bugs

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -2,12 +2,9 @@
 class Admin::DashboardController < Admin::BaseController
   
   def index
-    @total_participants_count = Participant.all.count
     @approved_theses_count = Thesis.approved.count
     @submited_theses_count = Thesis.submited.approved.count
-    @rated_count = Rating.rated.count
-    @unrate_count = Rating.unrate.count
-    @signup_date_chart = Participant.signup_date_chart
-    @nationality_chart = Participant.nationality_chart
+    @bookings_count = Booking.all.count
+    @hotels = Hotel.all
   end
 end
